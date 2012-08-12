079e11c205421ffc67507092980df64318a4d299
Andrew Stone <andrew.j.stone.1@gmail.com>
2012-08-12 18:26:07 -0400

    retrieve bucketized set members

--- test/stats.js
+++ test/stats.js
@@ -7,6 +7,7 @@ describe('sets', function() {
   var retentions = ['h', 'd', 'mo']; 
   var set = 'entries.contest1';
   var start = new Date();
+  var end = new Date(Date.now() + 1000*60*60*3);
 
   it('add 3 unique values', function(done) {
     async.forEach(['user1', 'user2', 'user3'], function(val, cb) {
@@ -15,11 +16,23 @@ describe('sets', function() {
   });
 
   it('get a set size of 3 for hour timeBucket', function(done) {
-    var end = new Date();
     stats.getSetSizes(set, 'h', start, end, function(err, sets) {
-      assert.equal(sets.length, 1);
+      assert.equal(sets.length, 4);
       assert.equal(typeof sets[0].bucket, 'string');
       assert.equal(sets[0].size, 3);
+      assert.equal(sets[1].size, 0);
+      assert.equal(sets[1].size, 0);
+      assert.equal(sets[1].size, 0);
+      done();
+    });
+  });
+
+  it('get the set in the first bucket and ensure it has 4 members', function(done) {
+    stats.getSetMembers(set, 'h', start, end, function(err, sets) {
+      assert(!err);
+      assert.equal(sets.length, 4);
+      assert(Array.isArray(sets[0].members));
+      assert.equal(sets[0].members.length, 3);
       done();
     });
   });
